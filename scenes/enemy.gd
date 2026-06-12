0600f8f05fabce8f312770b32a7384b2bc7e96a0
WINNVYN08 <152044695+WINNVYN08@users.noreply.github.com>
2026-06-12 12:34:32 +1200

    s

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,26 +1,41 @@
 extends CharacterBody3D
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
-var chasing = false 
+var chasing = false
 
-const SPEED = 40
+const SPEED = 5
 const JUMP_VELOCITY = 4.5
 
 
 func _physics_process(delta: float) -> void:
 	var destination = navigation_agent_3d.get_next_path_position()
 	var local_destination = destination - global_position
-	var direction = local_destination.normalized()
+		
 	
+	if chasing:
+		navigation_agent_3d.target_position = global.player.global_position
+		
+	elif navigation_agent_3d.is_navigation_finished():
+		var target = global_position
+		target.x += randf_range(-10, 10)
+		target.z += randf_range(-10, 10)
+		navigation_agent_3d.target_position = target
+	var direction = (destination - global_position).normalized()
+
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	velocity = direction* SPEED
 
-		
-	if navigation_agent_3d.is_navigation_finished():
-		var random_movement = Vector3.ZERO
-		random_movement.x = randf_range(-0.1, 0.1)
-		random_movement.z = randf_range(-0.1, 0.1)
-		navigation_agent_3d.set_target_position(random_movement)
-	
 	move_and_slide()
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	chasing == true
+	pass # Replace with function body.
+
+
+func _on_area_3d_area_exited(area: Area3D) -> void:
+	chasing == false
+	pass # Replace with function body.
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 var speed
 const WALK_SPEED = 5.0
-const SPRINT_SPEED = 100
+const SPRINT_SPEED = 10
 const JUMP_VELOCITY = 7
 const SENSITIVITY = 0.004
 
@@ -13,7 +13,7 @@ var t_bob = 0
 
 #fov variables
 const BASE_FOV = 75.0
-const FOV_CHANGE = 0
+const FOV_CHANGE = 2
 var can_dash = true
 
 # Gravity variable
@@ -26,6 +26,7 @@ var gravity = 10
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	global.player = self
 
 
 func _unhandled_input(event):
